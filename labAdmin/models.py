8b3409998c57636650a3b7f33b76af2f88d977f8
lorenzo <l.romagnoli@arduino.cc>
2016-04-04 15:09:24 +0200

    chancged app name

---
+++ labAdmin/models.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    username=models.CharField(max_length=200)
+#
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice_text = models.CharField(max_length=200)
+#     votes = models.IntegerField(default=0)
