eccf48fdc307bd267e1bff9ab67fcd787b6ebf2f
alessandromnc94 <alessandro.monaco.94@gmail.com>
2016-04-04 21:21:53 +0200

    Added new models

--- labAdmin/admin.py
+++ labAdmin/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+from polls.models import *
 
-from .models import User
-
+class MyModelAdmin(admin.ModelAdmin):
+    pass
 admin.site.register(User)
+admin.site.register(Usertype)
+admin.site.register(Device)
+admin.site.register(Devicetype)
+admin.site.register(Permission)
+admin.site.register(Logdoor)
+admin.site.register(Logdevice)
--- labAdmin/models.py
+++ labAdmin/models.py
@@ -1,13 +1,80 @@
-from __future__ import unicode_literals
-
 from django.db import models
+from django.utils import timezone
+from django import forms
 
-# Create your models here.
 
 class User(models.Model):
     username=models.CharField(max_length=200)
-#
-# class Choice(models.Model):
-#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#     choice_text = models.CharField(max_length=200)
-#     votes = models.IntegerField(default=0)
+    type=models.ForeignKey('Usertype')
+    signup=models.DateTimeField("Sign Up Date")
+    subscriptionEnd=models.DateTimeField("Subscription End Data")
+    needSubcription=models.BooleanField(default=True)
+    nfcId=models.IntegerField(null=True)
+
+class Usertype(models.Model):
+    name=models.CharField(max_length=200)
+    monday=models.BooleanField(default=False)
+    tuesday=models.BooleanField(default=False)
+    wednesday=models.BooleanField(default=False)
+    thursday=models.BooleanField(default=False)
+    friday=models.BooleanField(default=False)
+    saturday=models.BooleanField(default=False)
+    sunday=models.BooleanField(default=False)
+
+    HOUR_CHOICES = (
+        (0,0),
+        (1,1),
+        (2,2),
+        (3,3),
+        (4,4),
+        (5,5),
+        (6,6),
+        (7,7),
+        (8,8),
+        (9,9),
+        (10,10),
+        (11,11),
+        (12,12),
+        (13,13),
+        (14,14),
+        (15,15),
+        (16,16),
+        (17,17),
+        (18,18),
+        (19,19),
+        (20,20),
+        (21,21),
+        (22,22),
+        (23,23),
+        (24,24)
+    )
+
+    hourStart = models.IntegerField(choices=HOUR_CHOICES)
+    hourEnd = models.IntegerField(choices=HOUR_CHOICES)
+    isAdmin=models.BooleanField(default=False)
+
+class Device(models.Model):
+    name=models.CharField(max_length=200)
+    type=models.ForeignKey('Devicetype')
+
+class Devicetype(models.Model):
+        name=models.CharField(max_length=200)
+
+class Permission(models.Model):
+        level=models.IntegerField(default=0),
+        user=models.ForeignKey('User'),
+        devicetype=models.ForeignKey('Devicetype')
+
+class Logdoor(models.Model):
+    hour=models.DateTimeField("Hour of entering in FabLab")
+    doorOpened=models.BooleanField(default=False)
+    user=models.ForeignKey('User')
+
+class Logdevice(models.Model):
+    bootDevice=models.DateTimeField("Boot Device")
+    shutdownDevice=models.DateTimeField("Start work")
+    startWork=models.DateTimeField("Finish work")
+    finishWork=models.DateTimeField("Power Off Device")
+    hourlyCost=models.FloatField(null=True)
+    user=models.ForeignKey('User')
+    device=models.ForeignKey('Device')
