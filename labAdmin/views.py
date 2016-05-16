251457ae910ddc942117ad22905474d744391e93
Alessandro Monaco <alessandromnc94@github.com>
2016-05-16 12:45:40 +0200

    ò

--- labAdmin/views.py
+++ labAdmin/views.py
@@ -7,7 +7,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-form labAdmin import functions
+from labAdmin import functions
 # Create your views here.
 
 class LoginByNFC(APIView):
