fd1ba74c845db9740fe7bff6c9acee946a748884
Alessandro Monaco <alessandromnc94@github.com>
2016-05-16 11:23:56 +0200

    ok

--- labAdmin/admin.py
+++ labAdmin/admin.py
@@ -13,60 +13,60 @@ class UserAdmin(admin.ModelAdmin):
 admin.site.register(User, UserAdmin)
 
 class RoleAdmin(admin.ModelAdmin):
-    list_display = ('name', 'role_kind','valid')
+    list_display = ('name', 'role_kind','valid',)
     ordering = ('name',)
 
 admin.site.register(Role, RoleAdmin)
 
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ('name')
+    list_display = ('name','roles',)
     ordering = ('name',)
 
 admin.site.register(Group, GroupAdmin)
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name')
+    list_display = ('name',)
     ordering = ('name',)
 
 admin.site.register(Category, CategoryAdmin)
 
 
 class DeviceAdmin(admin.ModelAdmin):
-    list_display = ('name', 'hourlyCost', 'category', 'mac')
+    list_display = ('name', 'hourlyCost', 'category', 'mac',)
     ordering = ('name',)
 
 admin.site.register(Device, DeviceAdmin)
 
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = ('date', 'value', 'user')
+    list_display = ('date', 'value', 'user',)
     ordering = ('-date','user',)
 
 admin.site.register(Payment, PaymentAdmin)
 
 
 class LogAccessAdmin(admin.ModelAdmin):
-    list_display = ('datetime', 'user', 'opened')
+    list_display = ('datetime', 'user', 'opened',)
     ordering = ('-datetime','user',)
 
 admin.site.register(LogAccess,LogAccessAdmin)
 
 
 class LogDeviceAdmin(admin.ModelAdmin):
-    list_display = ('user', 'device', 'bootDevice', 'shutdownDevice', 'startWork', 'finishWork', 'inWorking', 'priceWork')
+    list_display = ('user', 'device', 'bootDevice', 'shutdownDevice', 'startWork', 'finishWork', 'inWorking', 'priceWork',)
     ordering = ('-bootDevice','-startWork',)
 
 admin.site.register(LogDevice, LogDeviceAdmin)
 
 class LogErrorAdmin(admin.ModelAdmin):
-    list_display = ('datetime', 'description', 'code')
+    list_display = ('datetime', 'description', 'code',)
     ordering = ('-bootDevice','-startWork',)
 
 admin.site.register(LogError, LogErrorAdmin)
 
 
 class TimeSlotAdmin(admin.ModelAdmin):
-    list_display = ('name', 'weekday_start', 'weekday_end', 'hour_start', 'hour_end')
+    list_display = ('name', 'weekday_start', 'weekday_end', 'hour_start', 'hour_end',)
     ordering = ('name',)
 
 admin.site.register(TimeSlot, TimeSlotAdmin)
