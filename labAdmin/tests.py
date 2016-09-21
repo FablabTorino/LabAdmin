f81ae1bc6c20a26e807d746e8706ff29f2c28471
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2016-09-21 11:59:43 +0200

    tests: rework tests to something that resembles a test
    Even if there's not tests actually :|

--- labAdmin/tests.py
+++ labAdmin/tests.py
@@ -1,32 +1,54 @@
-from django.test import TestCase
 from django.contrib.auth.models import User
-from labAdmin.models import *
+from django.test import TestCase
 from django.utils import timezone, dateparse
-# Create your tests here.
 
-UserProfile.objects.all().delete()
-Group.objects.all().delete()
-Role.objects.all().delete()
-
+from .models import (
+    Group, Role, TimeSlot, UserProfile
+)
 
 
-r1 = Role(name="Fablab Access", hour_start=dateparse.parse_time("8:0:0"), hour_end=dateparse.parse_time("23:0:0"), thursday=True, role_kind=0)
-r1.save()
-r2 = Role(name="Guest Access", hour_start=dateparse.parse_time("14:0:0"), hour_end=dateparse.parse_time("20:0:0"), thursday=True, role_kind=0)
-r2.save()
-r3 = Role(name="Fresatori", hour_start=dateparse.parse_time("14:0:0"), hour_end=dateparse.parse_time("20:0:0"), thursday=True, role_kind=0)
-r3.save()
+class TestLabAdmin(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        daily_timeslot = TimeSlot.objects.create(
+            name="thursday full day",
+            weekday_start=4,
+            weekday_end=4,
+            hour_start=dateparse.parse_time("8:0:0"),
+            hour_end=dateparse.parse_time("23:0:0"),
+        )
+        reduced_timeslot = TimeSlot.objects.create(
+            name="thursday reduced",
+            weekday_start=4,
+            weekday_end=4,
+            hour_start=dateparse.parse_time("14:0:0"),
+            hour_end=dateparse.parse_time("20:0:0"),
+        )
 
-g1 = Group(name="Fablab Guest")
-g1.save()
-g1.roles.add(r1,r2)
-g2 = Group(name="Guest")
-g2.save()
-g2.roles.add(r2)
-g3 = Group(name="Arduino")
+        fablab_role = Role.objects.create(
+            name="Fablab Access",
+            role_kind=0
+        )
+        fablab_role.time_slots.add(daily_timeslot)
 
-user = User.objects.create(username="alessandro.monaco")
-u = UserProfile(user=user, name="Alessandro Monaco",firstSignup=timezone.now(),lastSignup=timezone.now(),needSubcription=False,endSubcription=timezone.now())
-u.save()
-u.groups.add(g2)
-u.listRoles()
+        guest_role = Role.objects.create(
+            name="Guest Access",
+            role_kind=0
+        )
+        guest_role.time_slots.add(reduced_timeslot)
+
+        fab_guest_group = Group.objects.create(name="Fablab Guest")
+        fab_guest_group.roles.add(fablab_role, guest_role)
+        guest_group = Group.objects.create(name="Guest")
+        guest_group.roles.add(guest_role)
+
+        user = User.objects.create(username="alessandro.monaco")
+        u = UserProfile.objects.create(
+            user=user,
+            name="Alessandro Monaco",
+            firstSignup=timezone.now(),
+            lastSignup=timezone.now(),
+            needSubscription=False,
+            endSubscription=timezone.now()
+        )
+        u.groups.add(guest_group)
