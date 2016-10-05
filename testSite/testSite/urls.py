a46bb3e2dee3abe1d4cc4fd5134b495e8e5b1b8a
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2016-10-05 11:16:38 +0200

    labAdmin: Add crude login template
    We need to hook login url that does not ship with a template so if
    the project where we are installed does not have one provide
    ourselves.
    Hook django login and logout views to our testSite so we can
    test the oauth2 integration.

--- testSite/testSite/urls.py
+++ testSite/testSite/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^labAdmin/', include('labAdmin.urls')),
     url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+
+    url(r'^accounts/login/$', auth_views.login, {'template_name': 'labadmin/login.html'}, name='login'),
+    url(r'^accounts/logout/$', auth_views.logout, name='logout'),
 ]
