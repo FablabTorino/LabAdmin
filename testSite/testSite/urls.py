2fc41b7ac74b0e5583ddb6112a61f1141b848e53
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2016-10-05 15:47:34 +0200

    labAdmin: add link to reset password to login template
    And hook reset views on the test project

--- testSite/testSite/urls.py
+++ testSite/testSite/urls.py
@@ -25,4 +25,8 @@ urlpatterns = [
 
     url(r'^accounts/login/$', auth_views.login, {'template_name': 'labadmin/login.html'}, name='login'),
     url(r'^accounts/logout/$', auth_views.logout, name='logout'),
+    url(r'^accounts/password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^accounts/password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^accounts/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^accounts/reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 ]
