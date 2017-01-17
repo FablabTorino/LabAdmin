b824d03be96226700e538e8e19f31317f77fa543
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2017-01-17 12:35:17 +0100

    labAdmin: move token extraction from request into an helper
    As it will be handy for views needing a device.

--- labAdmin/permissions.py
+++ labAdmin/permissions.py
@@ -4,16 +4,24 @@ from rest_framework.authentication import get_authorization_header
 from .models import Device
 
 
+def get_token_from_request(request):
+    auth = get_authorization_header(request).split()
+
+    if len(auth) != 2:
+        return None
+
+    try:
+        token = auth[1].decode()
+    except UnicodeError:
+        return None
+
+    return token
+
+
 class DeviceTokenPermission(BasePermission):
     def has_permission(self, request, view):
-        auth = get_authorization_header(request).split()
-
-        if len(auth) != 2:
-            return False
-
-        try:
-            token = auth[1].decode()
-        except UnicodeError:
+        token = get_token_from_request(request)
+        if not token:
             return False
 
         try:
