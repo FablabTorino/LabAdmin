58507111b866364225ed172bcc5a82f55f335c50
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2017-01-24 11:03:51 +0100

    labAdmin: remove commented routes in urls

--- labAdmin/urls.py
+++ labAdmin/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+
 urlpatterns = [
     url(r'^opendoorbynfc/$', views.OpenDoorByNFC.as_view(), name="open-door-nfc"),
     url(r'^updateUsers/$', views.tempUpdateUser.as_view()),
@@ -10,11 +11,4 @@ urlpatterns = [
     url(r'^card/credits/$', views.CardCredits.as_view(), name='card-credits'),
     url(r'^device/use/start/$', views.DeviceStartUse.as_view(), name='device-use-start'),
     url(r'^device/use/stop/$', views.DeviceStopUse.as_view(), name='device-use-stop'),
-    # url(r'^nfc/(?P<nfc>.+)/$', views.NfcLogin.as_view()),
-    # url(r'^nfc/', views.NfcLogin.as_view()),
-    # url(r'^getpermission/', views.GetPermission.as_view()),
-    # url(r'^usedevicelist/', views.LogdeviceUseList.as_view()),
-    # url(r'^getToken/', views.GetTokenExample.as_view()),
-    # url(r'^users/', views.UserList.as_view()),
-    # url(r'^repeat/', views.Repeat.as_view()),
  ]
